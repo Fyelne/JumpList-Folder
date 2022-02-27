fe77eaa3c312c6c8d001da48960b2fe1776c467b
Fyelne <76248983+Fyelne@users.noreply.github.com>
2022-02-27 19:10:40 +0100

    Ajoutez des fichiers projet.

---
+++ CSPowerShellApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CSPowerShellApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(713, 421);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(84, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Parcourir";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chemin :";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(77, 422);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(630, 22);
+            this.txtPath.TabIndex = 2;
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(15, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(782, 393);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PowerShell";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ CSPowerShellApp/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace CSPowerShellApp
+{
+    public partial class Form1 : Form
+    {
+        List<string> listFiles = new List<string>();
+        JumpList jumpList;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(Object sender, EventArgs e)
+        {
+            // get where the app is running from
+            // create a jump list
+            JumpListCustomCategory category = new JumpListCustomCategory("Food");
+            category.AddJumpListItems(
+                new JumpListLink("calc.exe", "Crabs") { Arguments = "Crabs" },
+                new JumpListLink("notepad.exe", "Prawns") { Arguments = "Prawns" });
+            jumpList.AddCustomCategories(category);
+
+            jumpList.Refresh();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listFiles.Clear();
+            listView.Items.Clear();
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "All Files (*.*)|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = ofd.FileName;
+                    imageList.Images.Add(Icon.ExtractAssociatedIcon(ofd.FileName));
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    listFiles.Add(fi.FullName);
+                    listView.Items.Add(fi.Name, imageList.Images.Count - 1);
+                    
+                    JumpListCustomCategory manger = new JumpListCustomCategory("Manger");
+                    manger.AddJumpListItems(new JumpListLink(ofd.FileName, "Shrimps"));
+                    jumpList.AddCustomCategories(manger);
+                    jumpList.Refresh();
+                    
+                }
+            }
+        }
+    }
+}
